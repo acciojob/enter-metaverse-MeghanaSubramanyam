29f00903b3c73bae1df58e7d476df1aa86fcacfa
MeghanaSubramanyam <152699209+MeghanaSubramanyam@users.noreply.github.com>
2025-05-04 16:41:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 //your JS code here. If required.
 document.getElementById("enterBtn").addEventListener("click", function () {
-  document.getElementById("status").innerHTML = "<h1>Entered Metaverse</h1>";
-});
+  const statusElement = document.getElementById("status");
 
   // Replace <p> with <h1>Entered Metaverse</h1>
   const h1 = document.createElement("h1");
